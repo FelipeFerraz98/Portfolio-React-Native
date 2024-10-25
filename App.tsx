336ba8cf9708a165f95c481ecab79bb4b8226ec9
FelipeFerraz98 <felipeferraz1398@gmail.com>
2024-10-24 23:43:04 -0300

    Change StatusBar color

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
 import {TabsRoutes} from './src/routes/BottomTabs.routes';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
     <>
+      <StatusBar style="light" />
       <TabsRoutes />
     </>
   );
